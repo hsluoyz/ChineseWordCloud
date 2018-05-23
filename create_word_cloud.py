c32e4df6172ae5a5494aa25f71a65d22e40dc395
Yang Luo <hsluoyz@qq.com>
2018-05-23 22:03:25 +0800

    Output to "output.png".

--- create_word_cloud.py
+++ create_word_cloud.py
@@ -30,28 +30,29 @@ def main(input_filename):
 
     print '# of different words =', len(words_stat)
 
-    input_prefix = input_filename
-    if input_filename.find('.') != -1:
-        input_prefix = '.'.join(input_filename.split('.')[:-1])
+    do_job("love.png", words_stat)
 
-    for file in listdir(template_dir):
-        if file[-4:] != '.png' and file[-4:] != '.jpg':
-            continue
-        background_picture_filename = join(template_dir, file)
-        if isfile(background_picture_filename):
-            prefix = file.split('.')[0]
-            
-            bimg=imread(background_picture_filename)
-            wordcloud=WordCloud(font_path=font_filename,background_color='white',mask = bimg,max_font_size=600,random_state=100)
-            wordcloud=wordcloud.fit_words(dict(words_stat.head(4000).itertuples(index=False)))
 
-            # bimgColors=ImageColorGenerator(bimg)
-            # wordcloud.recolor(color_func=bimgColors)
+def do_job(file, words_stat):
+    background_picture_filename = join(template_dir, file)
+    if isfile(background_picture_filename):
+        prefix = file.split('.')[0]
 
-            output_filename = prefix + '_' + input_prefix + '.png'
+        bimg = imread(background_picture_filename)
+        wordcloud = WordCloud(font_path=font_filename,
+                              background_color='white',
+                              mask=bimg,
+                              max_font_size=600,
+                              random_state=100)
+        wordcloud = wordcloud.fit_words(dict(words_stat.head(4000).itertuples(index=False)))
 
-            print 'Saving', output_filename
-            wordcloud.to_file(output_filename)
+        # bimgColors=ImageColorGenerator(bimg)
+        # wordcloud.recolor(color_func=bimgColors)
+
+        output_filename = 'output.png'
+
+        print 'Saving', output_filename
+        wordcloud.to_file(output_filename)
 
 
 if __name__ == '__main__':
